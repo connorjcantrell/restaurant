6e90c0195542631261bd45757be0bb4847f8127b
Connor Cantrell <connorjcantrell@gmail.com>
2021-10-18 12:36:37 -0700

    Added helper function for JSONStorage class

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ class Project {
     remove(id) {
        this.tasks = this.tasks.filter(t => t.id !== id)
     }
+    // TODO: Pass in a function instead of field
     sort(field, reversed=false) {
         if (this.tasks.length === 0) return this.tasks
         if (this.tasks[0][field] === undefined) return this.tasks
@@ -39,49 +40,64 @@ class Project {
 }
 
 // Data storage and access using in localStorage
-class Storage {
+class JSONStorage {
     static get(key) {
-        const result = JSON.parse(localStorage.getItem(key))
-        if (result === null) return []
-        return result
+        const arr = JSON.parse(localStorage.getItem(key))
+        if (arr === null) return []
+        return arr
     }
     static post(key, value) {
-        // Add 'key' to 'projects', if not already
-        if (key !== 'projects') {
-            let projects = Storage.get('projects')
-            if (!projects.includes(key)) {
-                projects.push(key)
-                console.log(projects)
-                localStorage.setItem('projects', JSON.stringify(projects))
-            }
-        }
+        // Add 'key' to '__projects', if not already
+        JSONStorage.projectListManager(key)
         localStorage.setItem(key, JSON.stringify(value))
     }
     static delete(key) {
-        // Remove key from 'projects'
-        let projects = Storage.get('projects')
-        localStorage.setItem('projects', JSON.stringify(projects.filter(t => t !== key)))
+        // Remove key from '__projects'
+        let projects = JSONStorage.get('__projects')
+        localStorage.setItem('__projects', JSON.stringify(projects.filter(t => t !== key)))
         // Remove key from localStorage
         localStorage.removeItem(key)
     }
+    // Adds name to __projects key if it doesn't already exist 
+    static projectListManager(name) {
+        if (name !== '__projects') {
+            let projects = JSONStorage.get('__projects')
+            if (!projects.includes(name)) {
+                projects.push(name)
+                console.log(projects)
+                localStorage.setItem('__projects', JSON.stringify(projects))
+            }
+            return true
+        }
+        return false
+    }
 }
-
 // Project to Storage Interfacer
 class ProjectStorage {
-    static get(name) {
-        const json = Storage.get(name)
+    static getProjectNames() {
+        return JSONStorage.get('__projects')
+    }
+    static getProject(name) {
+        const json = JSONStorage.get(name)
         let tasks = json.map(t => {
             return new Task(t['title'], t['description'], t['dueDate'], t['priority'], t['status'], t['id'])
         })
         return new Project(name, tasks)
     }
     static post(project) {
-        Storage.post(project.name, project.tasks)
+        JSONStorage.post(project.name, project.tasks)
     }
     static delete(project) {
-        Storage.delete(project.name)
+        JSONStorage.delete(project.name)
     }
 }
+
+class UI {
+    constructor() {
+        this.projectNames = ProjectStorage.getProjectNames()
+    }
+}
+
 
 // Test
 let task = new Task('dishes', 'do the laundry', 'tomorrow', 'high', false)
@@ -95,3 +111,4 @@ d.add(task2)
 ProjectStorage.post(d)
 ProjectStorage.post(chores)
 ProjectStorage.get('default')
+
