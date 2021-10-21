89a38934663b9fc63323e683bbcb52100870c5a6
Connor Cantrell <connorjcantrell@gmail.com>
2021-10-21 14:07:06 -0700

    Manual testing

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,22 @@
 import _, { values } from 'lodash';
-import './UI'
+import Storage from './modules/Storage'
+import Task from './modules/Task'
+import Project from './modules/Project';
 
+let p = new Project('default')
+p.addTask(new Task('groceries', 'do them', 'now', 'high', false))
+p.addTask(new Task('laundry', 'do them', 'tomorrow', 'low', false))
+p.editDescription('Default tasks go here')
+let t1 = new Task('dishes', 'this should not be visible', 'never', 'low', false)
+p.addTask(t1)
+Storage.post(p)
 
+let p2 = new Project('Home Improvement')
+
+p2.addTask(new Task('groceries', 'do them', 'now', 'high', false))
+p2.addTask(new Task('laundry', 'do them', 'tomorrow', 'low', false))
+p2.editDescription('To get a nice house')
+Storage.post(p2)
+
+Storage.deleteProject(p2)
+// Storage.deleteTask(p, t1)
