e7cad098569d21e9508e3ce4119b34fb25d55cf1
Connor Cantrell <connorjcantrell@gmail.com>
2021-10-23 13:28:51 -0700

    Rendering home page

--- src/js/index.js
+++ src/js/index.js
@@ -2,21 +2,23 @@ import _, { values } from 'lodash';
 import Storage from './modules/Storage'
 import Task from './modules/Task'
 import Project from './modules/Project';
+import UI from "./modules/UI"
 
-let p = new Project('default')
-p.addTask(new Task('groceries', 'do them', 'now', 'high', false))
-p.addTask(new Task('laundry', 'do them', 'tomorrow', 'low', false))
-p.editDescription('Default tasks go here')
+let p1 = new Project('Default')
+p1.addTask(new Task('groceries', 'do them', 'now', 'high', false))
+p1.addTask(new Task('laundry', 'do them', 'tomorrow', 'low', false))
+p1.editDescription('Default tasks go here')
 let t1 = new Task('dishes', 'this should not be visible', 'never', 'low', false)
-p.addTask(t1)
-Storage.post(p)
+p1.addTask(t1)
+Storage.post(p1)
 
-let p2 = new Project('Home Improvement')
+let p2 = new Project('Home Improvement YO')
 
 p2.addTask(new Task('groceries', 'do them', 'now', 'high', false))
 p2.addTask(new Task('laundry', 'do them', 'tomorrow', 'low', false))
 p2.editDescription('To get a nice house')
 Storage.post(p2)
 
-Storage.deleteProject(p2)
-// Storage.deleteTask(p, t1)
+UI.displayHeader()
+UI.displayProjects()
+// UI.clearContent()
